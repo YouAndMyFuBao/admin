21e2b214ebe82dcf737015b7ce62c70278121bc6
anonymousrecords <a.Records2022@gmail.com>
2024-02-20 15:45:12 +0900

    feat : 로그인 페이지 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { pretendardFont } from '../../public/fonts/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <HydrationBoundary state={pageProps.dehydratedState}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <Component {...pageProps} />
+        <Component className={pretendardFont.className} {...pageProps} />
       </HydrationBoundary>
     </QueryClientProvider>
   );
