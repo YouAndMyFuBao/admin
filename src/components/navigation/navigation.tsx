beaf64081395f86ca150508afa76a162358c562c
anonymousrecords <a.Records2022@gmail.com>
2024-04-17 22:33:37 +0900

    feat : 네비게이션 컴포넌트 구현

---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,56 @@
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+
+type NavigationType = {
+  id: number;
+  name: string;
+  path: string;
+};
+
+const NavigationList: NavigationType[] = [
+  { id: 0, name: '데일리 미션', path: '/daily' },
+  { id: 1, name: '닉네임 규정', path: '/nickname' },
+  { id: 2, name: '문의', path: '/inquire' },
+];
+
+export default function Navigation() {
+  const router = useRouter();
+  return (
+    <div css={WrapperStyle}>
+      {NavigationList.map((item) => (
+        <button
+          key={item.id}
+          css={ButtonStyle}
+          onClick={() => {
+            router.push(`/${item.path}`);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+const WrapperStyle = css({
+  width: '260px',
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: ' #64A47A',
+});
+
+const ButtonStyle = css({
+  width: '100%',
+  height: '50px',
+  border: 'none',
+  borderRadius: '10px',
+  marginBottom: '10px',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: '#f0f0f0',
+    fontWeight: 'bold',
+  },
+})
