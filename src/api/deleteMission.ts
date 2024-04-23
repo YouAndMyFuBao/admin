36991e7f101da47d83aa0edad25aafc079480199
anonymousrecords <a.Records2022@gmail.com>
2024-04-23 20:51:54 +0900

    feat : 데일리 미션 삭제 기능 구현

--- src/api/deleteMission.ts
+++ src/api/deleteMission.ts
@@ -3,6 +3,7 @@ import { baseAxios } from './baseAxios';
 
 export const deleteMission = async (missionId: number) => {
   const response = await baseAxios.delete<APIResponse<string>>(`/api/v1/admin/mission/${missionId}`);
+  console.log(response.data);
   return response.data;
 };
 
